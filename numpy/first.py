65cc07eda13418d6a707da1617b8e7cbf98eaa4d
RajviDave <rajvidave22@gmail.com>
2026-03-15 22:58:33 +0530

    slicing

--- numpy/first.py
+++ numpy/first.py
@@ -2,32 +2,35 @@ import numpy as np
 
 #conversion
 array=(1,2,3,4,5)
-print(type(array))
+# print(type(array))
 
 array=np.array([1,2,3,4,5])
-print(type(array))
+# print(type(array))
 
 #dimentions in array
 
 #zero dimention
 arr1=np.array(30)
-print(arr1.ndim)
+# print(arr1.ndim)
 
 #one dimention
 arr2=np.array([1,2,3,4,5])
-print(arr2.ndim)
+# print(arr2.ndim)
 
 #two dimentions
 arr3=np.array([[1,2,3],[4,5,6]])
-print(arr3.ndim)
-print(arr3.shape)
+# print(arr3.ndim)
+# print(arr3.shape)
 
 #threeD dimentions 
 arr4=np.array([[[1,2,3],[4,5,6],[7,8,9]],[[1,2,3],[4,5,6],[7,8,9]]])
-print(arr4)
-print(arr4[1,1,1])
+# print(arr4)
+# print(arr4[1,1,1])
 
 #multiple dimentions 
 arr5=np.zeros((3,4,3,5,5))
-print(arr5[1,2,2,2,1])
+# print(arr5[1,2,2,2,1])
 
+normal_array=[1,2,3,4,5,6,7]
+
+print(normal_array[-4:-1])
