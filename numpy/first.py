caef7ab4c9ef0b1cf7573713c8eb7450d707f180
RajviDave <rajvidave22@gmail.com>
2026-03-15 21:45:27 +0530

    three D array

--- numpy/first.py
+++ numpy/first.py
@@ -21,3 +21,8 @@ print(arr2.ndim)
 arr3=np.array([[1,2,3],[4,5,6]])
 print(arr3.ndim)
 print(arr3.shape)
+
+#threeD dimentions 
+arr4=np.array([[[1,2,3],[4,5,6],[7,8,9]],[[1,2,3],[4,5,6],[7,8,9]]])
+print(arr4.ndim)
+print(arr4.shape)
