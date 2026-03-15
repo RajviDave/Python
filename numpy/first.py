0331e8a6af571190ff57b383bc20e49cd60a8411
RajviDave <rajvidave22@gmail.com>
2026-03-15 21:24:42 +0530

    nulti ddimentional array

--- numpy/first.py
+++ numpy/first.py
@@ -1,7 +1,23 @@
 import numpy as np
 
+#conversion
 array=(1,2,3,4,5)
 print(type(array))
 
 array=np.array([1,2,3,4,5])
 print(type(array))
+
+#dimentions in array
+
+#zero dimention
+arr1=np.array(30)
+print(arr1.ndim)
+
+#one dimention
+arr2=np.array([1,2,3,4,5])
+print(arr2.ndim)
+
+#two dimentions
+arr3=np.array([[1,2,3],[4,5,6]])
+print(arr3.ndim)
+print(arr3.shape)
