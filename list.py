32a178a85ba6d1dfcc504ac55ae100a6f5eb796e
RajviDave <rajvidave22@gmail.com>
2026-04-08 13:53:04 +0530

    list operations done

--- list.py
+++ list.py
@@ -1,11 +1,18 @@
-import pandas as pd
+one="rajvi"
+two="dave"
+three="positive"
+four="negative"
 
-list=['b','a','c','d','e','f','g']
+final=[one,two,three,four]
+#print(final)
 
-list.sort(reverse=True)
+list=[1,2,3,4,5,6,7,8,9,0]
 
-list.insert(1,'z')
-list.insert(5,'u')
-
+#list functions
+list.append(12)# add element to the end of list
+list.sort()#it will sort the list in ascending
+list.sort(reverse=True)#it will sort it descending
+list.reverse()#it will reverse the order in with list is stored
+list.insert(6,20)#insert element at specific index
+print(len(list))#will return length of list
 print(list)
-
