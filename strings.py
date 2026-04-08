8dc2dc9611c02e79069917821f44b2b58c8bcdbb
RajviDave <rajvidave22@gmail.com>
2026-04-08 13:30:43 +0530

    string basic operations

--- strings.py
+++ strings.py
@@ -1,8 +1,20 @@
 first_string="rajvi's"
-print(first_string)
+#print(first_string)
 
 second_string="""first office"""
-print(second_string)
+#print(second_string)
 
 final=first_string+second_string
-print(final)
+#print(final)
+
+str1="I am a coder"
+#print(str[:6])
+
+#string functions
+#print(str1.endswith("er"))#checks if string ends with some pattern and answers in true or false
+
+str2="i am coder and i am a research enthu"
+print(str2.capitalize())#it will capitalize the string
+print(str2.replace('a','o'))#it will replace the old value with new value (first old value to second new value)
+print(str2.find('coder'))#it will find weather the word or letter exist in string or not "it will return first index of occurance"
+print(str2.count("a"))
