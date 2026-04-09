3eeb964c959746300df07ac29e22e31470e2c5fe
RajviDave <rajvidave22@gmail.com>
2026-04-09 22:38:17 +0530

    dictionary data

--- dictionary.py
+++ dictionary.py
@@ -2,3 +2,5 @@
 #are mutable
 #does not allow duplicate keys
 
+students={'rajvi':38, 41:'dhruvi', 'priyanshi':43,'subjects': ['math','science','english'],'dict':{'sem':1,'next':3}}
+print(type(students))
