94390d07afc8e08f09396b6236069bc65a200d2c
RajviDave <rajvidave22@gmail.com>
2026-04-09 22:45:33 +0530

    all dictionary functions

--- dictionary.py
+++ dictionary.py
@@ -3,4 +3,12 @@
 #does not allow duplicate keys
 
 students={'rajvi':38, 41:'dhruvi', 'priyanshi':43,'subjects': ['math','science','english'],'dict':{'sem':1,'next':3}}
-print(type(students))
+#print(type(students))
+
+print(students.keys()) #it will print all the keys of dictionary
+print(students.values())#it will print all values
+print(students.items())#it will print all key and value pairs
+print(students.get('rajvi'))
+print(students['rajvi'])
+students.update({'umang':25})
+print(students)
