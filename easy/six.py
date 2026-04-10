2436b0141bfac1833698e760920875a5d1529bc9
RajviDave <rajvidave22@gmail.com>
2026-04-10 10:43:22 +0530

    six-easy

---
+++ easy/six.py
@@ -0,0 +1,7 @@
+# Store marks of students:
+# Rahul → 85, Priya → 92, Amit → 78
+# Print the marks of Priya.
+
+marks={'Rahul':85,'priya':92,'amit':78}
+print(marks.get('priya'))
+print(marks['priya'])
