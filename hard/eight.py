8397aa3f78a5d86c54e059fa11c93f7c08c69142
RajviDave <rajvidave22@gmail.com>
2026-04-11 17:08:48 +0530

    hard questions done

--- hard/eight.py
+++ hard/eight.py
@@ -2,3 +2,10 @@
 #[(1,2), (3,4), (5,6)].
 
 nums=[(1,2), (3,4), (5,6)]
+
+result=[]
+for tup in nums:
+    for data in tup:
+        result.append(data)
+
+print(result)
--- hard/nine.py
+++ hard/nine.py
@@ -4,8 +4,6 @@
 set1={1,2,3,4}
 set2={3,4,5,6}
 
-common_element=set1.intersection(set2)
-union_element=set1.union(set2)
-final_answer=common_element-union_element
+result = set1.symmetric_difference(set2)
 
-print(final_answer)
+print(result)
