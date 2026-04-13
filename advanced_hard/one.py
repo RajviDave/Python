ab55c0ec7e438f6f59626d3166def24635c0efc5
RajviDave <rajvidave22@gmail.com>
2026-04-13 10:41:13 +0530

    function first

---
+++ advanced_hard/one.py
@@ -0,0 +1,6 @@
+#Group words by their first letter from
+#["apple", "ant", "banana", "bat", "cat"].
+
+words=["apple", "ant", "banana", "bat", "cat"]
+
+groups={}
---
+++ functions/one.py
@@ -0,0 +1,9 @@
+#WAF to print length of list.
+
+def length(l):
+    return(len(l))
+
+nums=[1,2,3,4,5]
+
+len=length(nums)
+print(len)
