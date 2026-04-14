dd8b5d7dfa6f391164d83ba79576eeb77c387153
RajviDave <rajvidave22@gmail.com>
2026-04-14 15:47:20 +0530

    file lines

--- file_io/one.py
+++ file_io/one.py
@@ -1,10 +1,17 @@
-f=open("demo.txt",'r')
-data=f.read()
+f=open("demo.txt","r") # if we do not mention mode here it will defalt consider 'r' read mode.
+data=f.read(5)#read(n) will read first n characters and we do not mention 'n' then it will read whole text
 print(data)
-b=f.read(10)#it will only read 5 characters
-print(b)
+print(type(data))
 f.close()
 
-a=open("demo.txt","r")
-abc=a.readline(1)
-print(abc)
+a=open("demo.txt",'r')
+data=a.readline()#this will read file line by line
+print(data)
+
+line2=a.readline()
+print(line2)#to read second line we will agaiin write readline() function
+
+line3=a.readline()
+print(line3)
+
+a.close()
